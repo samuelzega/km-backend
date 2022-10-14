10c09479561ead4951a850a3fc491645e08177eb
samuelzega <samuel.zegaa@gmail.com>
2022-10-14 19:22:08 +0700

    update samuel.js

--- samuel.js
+++ samuel.js
@@ -1,1 +1,5 @@
-let name = "samuel"
+let fullname = "samuel zega"
+let classType = "backend"
+let className = "be-0"
+
+console.log(`my name is ${fullname}, im joining ${classType} class, and i am in ${className}`);
